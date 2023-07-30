f957a803a65667a12882ef12c819a56a334255d1
KomarovVladimir <komarov2142@gmail.com>
2023-07-30 14:19:10 +0300

    Add a background and paddings

--- src/App.js
+++ src/App.js
@@ -2,37 +2,39 @@ import "./App.css";
 
 export default function App() {
     return (
-        <div class="flex gap-4 flex-nowrap overflow-x-auto md:flex-wrap ">
-            <div class="sticky top-0 left-0 flex-shrink-0 w-[calc(100%/5-(1rem*4/5))] h-32 bg-teal-200">
-                01
+        <main class="h-screen bg-[#150F0D] pt-[34px] pl-[50px] pr-[50px]">
+            <div class="flex gap-4 flex-nowrap overflow-x-auto md:flex-wrap">
+                <div class="sticky top-0 left-0 flex-shrink-0 w-[calc(100%/5-(1rem*4/5))] h-32 bg-teal-200">
+                    01
+                </div>
+                <div class="sticky top-0 left-0 flex-shrink-0 w-[calc(100%/5-(1rem*4/5))] h-32 bg-teal-200">
+                    02
+                </div>
+                <div class="sticky top-0 left-0 flex-shrink-0 w-[calc(100%/5-(1rem*4/5))] h-32 bg-teal-200">
+                    03
+                </div>
+                <div class="sticky top-0 left-0 flex-shrink-0 w-[calc(100%/5-(1rem*4/5))] h-32 bg-teal-200">
+                    04
+                </div>
+                <div class="sticky top-0 left-0 flex-shrink-0 w-[calc(100%/5-(1rem*4/5))] h-32 bg-teal-200">
+                    05
+                </div>
+                <div class="sticky top-0 left-0 flex-shrink-0 w-[calc(100%/5-(1rem*4/5))] h-32 bg-teal-200">
+                    06
+                </div>
+                <div class="sticky top-0 left-0 flex-shrink-0 w-[calc(100%/5-(1rem*4/5))] h-32 bg-teal-200">
+                    07
+                </div>
+                <div class="sticky top-0 left-0 flex-shrink-0 w-[calc(100%/5-(1rem*4/5))] h-32 bg-teal-200">
+                    08
+                </div>
+                <div class="sticky top-0 left-0 flex-shrink-0 w-[calc(100%/5-(1rem*4/5))] h-32 bg-teal-200">
+                    09
+                </div>
+                <div class="sticky top-0 left-0 flex-shrink-0 w-[calc(100%/5-(1rem*4/5))] h-32 bg-teal-200">
+                    10
+                </div>
             </div>
-            <div class="sticky top-0 left-0 flex-shrink-0 w-[calc(100%/5-(1rem*4/5))] h-32 bg-teal-200">
-                02
-            </div>
-            <div class="sticky top-0 left-0 flex-shrink-0 w-[calc(100%/5-(1rem*4/5))] h-32 bg-teal-200">
-                03
-            </div>
-            <div class="sticky top-0 left-0 flex-shrink-0 w-[calc(100%/5-(1rem*4/5))] h-32 bg-teal-200">
-                04
-            </div>
-            <div class="sticky top-0 left-0 flex-shrink-0 w-[calc(100%/5-(1rem*4/5))] h-32 bg-teal-200">
-                05
-            </div>
-            <div class="sticky top-0 left-0 flex-shrink-0 w-[calc(100%/5-(1rem*4/5))] h-32 bg-teal-200">
-                06
-            </div>
-            <div class="sticky top-0 left-0 flex-shrink-0 w-[calc(100%/5-(1rem*4/5))] h-32 bg-teal-200">
-                07
-            </div>
-            <div class="sticky top-0 left-0 flex-shrink-0 w-[calc(100%/5-(1rem*4/5))] h-32 bg-teal-200">
-                08
-            </div>
-            <div class="sticky top-0 left-0 flex-shrink-0 w-[calc(100%/5-(1rem*4/5))] h-32 bg-teal-200">
-                09
-            </div>
-            <div class="sticky top-0 left-0 flex-shrink-0 w-[calc(100%/5-(1rem*4/5))] h-32 bg-teal-200">
-                10
-            </div>
-        </div>
+        </main>
     );
 }
