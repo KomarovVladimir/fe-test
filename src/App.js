bc2ca423691863b420d0b1a1693b3d164e440ac0
KomarovVladimir <komarov2142@gmail.com>
2023-07-30 11:42:38 +0300

    Add a grid

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
 import "./App.css";
 
 export default function App() {
-    return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+    return (
+        <div class="grid gap-x-8 gap-y-4 md:grid-cols-3 gap-4 grid-flow-col md:grid-flow-row overflow-x-auto">
+            <div class="sticky h-32 w-64 md:w-auto bg-teal-200 top-0 left-0">
+                01
+            </div>
+            <div class="sticky h-32 w-64 md:w-auto bg-teal-200 top-0 left-0">
+                02
+            </div>
+            <div class="sticky h-32 w-64 md:w-auto bg-teal-200 top-0 left-0">
+                03
+            </div>
+            <div class="sticky h-32 w-64 md:w-auto bg-teal-200 top-0 left-0">
+                04
+            </div>
+            <div class="sticky h-32 w-64 md:w-auto bg-teal-200 top-0 left-0">
+                05
+            </div>
+            <div class="sticky h-32 w-64 md:w-auto bg-teal-200 top-0 left-0">
+                06
+            </div>
+        </div>
+    );
 }
