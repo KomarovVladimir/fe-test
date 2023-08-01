a79c0d414ae3323e4b3802c584844bbdfcd499d7
KomarovVladimir <komarov2142@gmail.com>
2023-08-01 18:12:06 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import { descriptions } from "./data";
 
 const CardsWrapper = styled.div`
     position: relative;
+    display: flex;
+    flex-wrap: nowrap;
+    margin: -6px;
+    scroll-snap-type: x var(--tw-scroll-snap-strictness);
     overflow-x: auto;
     overflow-y: hidden;
     -webkit-mask: linear-gradient(to left, transparent 25%, red 45%);
@@ -14,15 +18,19 @@ const CardsWrapper = styled.div`
         width: 0px;
         background: transparent;
     }
-    @media (min-width: 1400px) {
+    @media (min-width: 768px) {
+    }
+    @media (min-width: 1700px) {
+        flex-wrap: wrap;
         -webkit-mask: none;
         mask: none;
     }
 `;
 
+//className="flex flex-nowrap -m-[6px] snap-x md:-m-2 2xl:flex-wrap"
 export const App = () => (
     <main className="min-h-screen bg-[#150F0D] py-[16px] px-[10px] md:py-[29px] md:px-[20px] 2xl:py-[34px] 2xl:px-[50px]">
-        <CardsWrapper className="flex flex-nowrap -m-[6px] snap-x md:-m-2 2xl:flex-wrap">
+        <CardsWrapper>
             {descriptions.map(({ title, description, price, image }, index) => (
                 <Card
                     key={index}
