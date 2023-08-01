42123c975b6acb5ce9cf5aae287fdd15b517ddb6
KomarovVladimir <komarov2142@gmail.com>
2023-08-01 15:01:31 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,41 +2,46 @@ import { styled } from "styled-components";
 
 import "./App.css";
 import { Card, LinkCard } from "./components";
-import { descriptions } from "./data"
+import { descriptions } from "./data";
 
 const CardsWrapper = styled.div`
-   position: relative;
-    overflow: hidden;
+    position: relative;
+    overflow-x: auto;
+    overflow-y: hidden;
     z-index: 1;
     &:after {
         content: "";
-        position: absolute;
+        position: sticky;
         display: block;
-        background: linear-gradient(270deg, #151416 0%, rgba(21, 20, 22, 0.00) 100%);
-        width: 84px;
-        height: 100%;
+        background: linear-gradient(
+            270deg,
+            #151416 0%,
+            rgba(21, 20, 22, 0) 100%
+        );
+        padding-left: 84px;
         top: 0;
-        right: -15px;
+        right: 0;
         z-index: 1000;
+    }
+    &::-webkit-scrollbar {
+        width: 0px;
+        background: transparent;
     }
 `;
 
-
 export const App = () => (
     <main className="min-h-screen bg-[#150F0D] pt-[34px] pb-[34px] pl-[50px] pr-[50px]">
-        <CardsWrapper className="2xl:after:invisible">
-            <div className="flex flex-nowrap 2xl:flex-wrap -m-[6px] md:-m-2 overflow-x-auto snap-x">
-                {descriptions.map(({ title, description, price, image }, index) => (
-                        <Card
-                            key={index}
-                            promoText="Save up to 15%"
-                            background={image}
-                            topSeller={index > 1}
-                            {...{title, description, price}}
-                        />
-                    ))}
-                <LinkCard />
-            </div>
+        <CardsWrapper className="flex flex-nowrap 2xl:flex-wrap -m-[6px] md:-m-2 snap-x 2xl:after:invisible">
+            {descriptions.map(({ title, description, price, image }, index) => (
+                <Card
+                    key={index}
+                    promoText="Save up to 15%"
+                    background={image}
+                    topSeller={index > 1}
+                    {...{ title, description, price }}
+                />
+            ))}
+            <LinkCard />
         </CardsWrapper>
     </main>
 );
